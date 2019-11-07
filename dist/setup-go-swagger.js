cf3f089a500b509374cb2c4a156dd6dbc8bad7dc
Grumpy Programmer <very.grumpy.programmer@gmail.com>
2019-11-07 13:05:55 +0100

    feat: add actions

---
+++ dist/setup-go-swagger.js
@@ -0,0 +1,105 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const tc = __importStar(require("@actions/tool-cache"));
+const os = __importStar(require("os"));
+const RestClient_1 = require("typed-rest-client/RestClient");
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const version = core.getInput('go-swagger-version');
+            yield getGoSwagger(version);
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+function getGoSwagger(version) {
+    return __awaiter(this, void 0, void 0, function* () {
+        version = yield getVersion(version);
+        core.info(`version: ${version}`);
+        const path = yield ensure(version);
+        core.info(`path: ${path}`);
+        core.addPath(path);
+    });
+}
+function getVersion(version) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (version) {
+            return version;
+        }
+        return getLatestVersion();
+    });
+}
+function getLatestVersion() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new RestClient_1.RestClient('nodejs');
+        const release = yield client.get('https://api.github.com/repos/go-swagger/go-swagger/releases/latest');
+        if (release.result) {
+            return release.result.tag_name;
+        }
+        return '';
+    });
+}
+function ensure(version) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const cachePath = tc.find('go-swagger', version);
+        if (cachePath) {
+            core.info(`cache found: ${cachePath}`);
+            return cachePath;
+        }
+        const url = getUrl(version);
+        core.info(`download from: ${url}`);
+        const downloadPath = yield tc.downloadTool(url);
+        core.info(`downloaded to: ${downloadPath}`);
+        const cacheSavePath = yield tc.cacheFile(downloadPath, 'swagger', 'go-swagger', version);
+        core.info(`cached to: ${cacheSavePath}`);
+        return cacheSavePath;
+    });
+}
+function getUrl(version) {
+    const platform = getPlatform();
+    const arch = getArch();
+    return `https://github.com/go-swagger/go-swagger/releases/download/${version}/swagger_${platform}_${arch}`;
+}
+function getPlatform() {
+    const platform = os.platform();
+    switch (platform) {
+        case 'linux':
+        case 'darwin':
+            return platform;
+        case 'win32':
+            return 'windows';
+        default:
+            return platform;
+    }
+}
+function getArch() {
+    const arch = os.arch();
+    switch (arch) {
+        case 'x64':
+            return 'amd64';
+        case 'x32':
+            return '386';
+        default:
+            return arch;
+    }
+}
+run();
---
+++ src/setup-go-swagger.ts
@@ -0,0 +1,109 @@
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as os from 'os';
+import { RestClient } from 'typed-rest-client/RestClient';
+
+interface Release {
+  tag_name: string;
+}
+
+async function run() {
+  try {
+    const version = core.getInput('go-swagger-version');
+
+    await getGoSwagger(version);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function getGoSwagger(version: string) {
+
+  version = await getVersion(version);
+
+  core.info(`version: ${version}`);
+
+  const path = await ensure(version);
+
+  core.info(`path: ${path}`);
+
+  core.addPath(path);
+}
+
+async function getVersion(version: string): Promise<string> {
+  if (version) {
+    return version;
+  }
+
+  return getLatestVersion();
+}
+
+async function getLatestVersion(): Promise<string> {
+  const client = new RestClient('nodejs');
+  const release = await client.get<Release>('https://api.github.com/repos/go-swagger/go-swagger/releases/latest');
+
+  if (release.result) {
+    return release.result.tag_name;
+  }
+
+  return '';
+}
+
+async function ensure(version: string): Promise<string> {
+  const cachePath = tc.find('go-swagger', version);
+
+  if (cachePath) {
+    core.info(`cache found: ${cachePath}`);
+    return cachePath;
+  }
+
+  const url = getUrl(version);
+  core.info(`download from: ${url}`);
+
+  const downloadPath = await tc.downloadTool(url);
+  core.info(`downloaded to: ${downloadPath}`);
+
+  const cacheSavePath = await tc.cacheFile(downloadPath, 'swagger', 'go-swagger', version);
+  core.info(`cached to: ${cacheSavePath}`);
+
+  return cacheSavePath;
+}
+
+function getUrl(version: string): string {
+  const platform = getPlatform();
+  const arch = getArch();
+
+  return `https://github.com/go-swagger/go-swagger/releases/download/${version}/swagger_${platform}_${arch}`;
+}
+
+function getPlatform(): string {
+  const platform = os.platform();
+
+  switch (platform) {
+    case 'linux':
+    case 'darwin':
+      return platform;
+    case 'win32':
+      return 'windows';
+    default:
+      return platform;
+  }
+}
+
+function getArch(): string {
+  const arch = os.arch();
+
+  switch (arch) {
+    case 'x64':
+      return 'amd64';
+
+    case 'x32':
+      return '386';
+
+    default:
+      return arch;
+  }
+}
+
+run();
